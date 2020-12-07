e2b2ff64fddf21ab861706cf33ba7e827277e5b2
Joonas Jokinen <46967273+jnsjknn@users.noreply.github.com>
2020-12-07 11:52:52 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ echo -e "${CYAN}[STATUS] Update finished"
 echo "[STATUS] Setting up automatic updates.."
 sudo cp ./auto-update.sh /usr/bin/auto-update.sh
 sudo chmod +x /usr/bin/auto-update.sh
-sudo cp ./weekly-reboot.sh /usr/bin/monthly-reboot.sh
+sudo cp ./monthly-reboot.sh /usr/bin/monthly-reboot.sh
 sudo chmod +x /usr/bin/monthly-reboot.sh
 sudo crontab -l > newCron
 echo "@reboot sudo bash /usr/bin/auto-update.sh &" >> newCron
